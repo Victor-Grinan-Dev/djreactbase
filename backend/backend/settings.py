e5e99cdfa9465c09f698b38b455acbfd9573ec61
Victor <victorgrinan@gmail.com>
2025-02-18 15:05:57 +0200

    fix cors setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,14 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://yourfrontend.com",
+]
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
+  
+
+  const fetchBooks = async () => {
+    try{
+      const response = await fetch('http://127.0.0.1:8000/api/books/');
+      const data = await response.json()
+      console.log(data)
+    }catch(error){
+      console.log(error)
+    }
+  }
 
   return (
     <>
