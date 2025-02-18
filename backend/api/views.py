ed62b12740c5c5ce40402465e99927333456ab38
Victor <victorgrinan@gmail.com>
2025-02-18 14:33:02 +0200

    create models, serializer, views and urls for books

---
+++ backend/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializer import BookSerializer
+
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all() 
+    serialized_data = BookSerializer(books, many=True).data
+    return Response(serialized_data)
